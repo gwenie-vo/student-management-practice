f8255c5308e97b5dcd3a860afd6a4ada92f13c93
Huynh Vo <huynh.vo@asnet.com.vn>
2016-02-17 17:48:38 +0700

    Do the Angular2 - Form example

---
+++ angular2-forms/app/hero-form.component.ts
@@ -0,0 +1,22 @@
+import {Component} from 'angular2/core';
+import {NgForm} from 'angular2/common';
+import {Hero} from './hero';
+
+@Component({
+  selector: 'hero-form',
+  templateUrl: 'app/hero-form.component.html'
+})
+
+export class HeroFormComponent {
+  powers = ['Really Smart', 'Super Flexible', 'Super Hot', 'Weather Changer'];
+
+  model = new Hero(18, 'Elizabeth', this.powers[0], 'Chuck overstreet');
+
+  submitted = false;
+
+  onSubmit() { this.submitted = true; }
+
+  //TODO: Remove this when we're done
+
+  get diagnostic() { return JSON.stringify(this.model);}
+}
