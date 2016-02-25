13ff9c145f99d44bb6a053ac487f0f75f65ce458
Huynh Vo <huynh.vo@asnet.com.vn>
2016-02-25 18:39:18 +0700

    Template syntax examples

--- template-syntax/app/app.component.js
+++ template-syntax/app/app.component.js
@@ -23,13 +23,18 @@ System.register(['angular2/core'], function(exports_1, context_1) {
                     this.heroImageUrl = "http://ww1.prweb.com/prfiles/2014/04/10/11752526/gI_134971_best-image-web-hosting.png";
                     this.tittle = "Template syntax";
                 }
+                AppComponent.prototype.toggleSelected = function () {
+                    this.isSelected = !this.isSelected;
+                    console.log("this.isSelected:", this.isSelected);
+                };
                 AppComponent.prototype.getVal = function () {
+                    var a = 40 + 4, b = "Text string";
                     return "Message";
                 };
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: "\n  <h3>\n    {{title}}\n    <img src=\"{{heroImageUrl}}\" style=\"height:350px\">\n  </h3>\n  <!-- \"The sum of 1 + 1 is 2\" -->\n  <p>The sum of 1 + 1 is {{1 + 1}} + </p>\n\n  <!-- \"The sum of 1 + 1 is not 4\" -->\n  <p>The sum of 1 + 1 is not {{1 + 1 + \" \" + getVal()}}</p>\n  <button [disabled]=\"isUnchanged\">Save</button>\n  "
+                        template: "\n  <h3>\n    {{title}}\n    <img src=\"{{heroImageUrl}}\" style=\"height:350px\">\n  </h3>\n  <!-- \"The sum of 1 + 1 is 2\" -->\n  <p>The sum of 1 + 1 is {{1 + 1}} + </p>\n\n  <!-- \"The sum of 1 + 1 is not 4\" -->\n  <p>The sum of 1 + 1 is not {{2 + 2 + \" \" + getVal()}}</p>\n  <button (click)=\"toggleSelected()\">Save</button>\n  <div id=\"testNgClass1\" [ngClass] = \"{selected: isSelected, huynh: !isSelected, abc: !isSelected}\">1</div>\n  <div class=\"abc\" [class.trung]=\"isSelected\"></div>\n  <button [title]=\"tittle\">help</button>\n\n<!-- isSpecial is true -->\n<div [class.hidden]=\"!isSpecial\">Show with class</div>\n<div [class.hidden]=\"isSpecial\">Hide with class</div>\n\n<!-- HeroDetail is in the DOM but hidden -->\n<hero-detail [class.hidden]=\"isSpecial\"></hero-detail>\n\n<div [style.display]=\"isSpecial ? 'block' : 'none'\">Show with style</div>\n<div [style.display]=\"isSpecial ? 'none'  : 'block'\">Hide with style</div>\n\n<span [ngSwitch]=\"toeChoice\">\n  <span *ngSwitchWhen=\"'Eenie'\">Eenie</span>\n  <span *ngSwitchWhen=\"'Meanie'\">Meanie</span>\n  <span *ngSwitchWhen=\"'Miney'\">Miney</span>\n  <span *ngSwitchWhen=\"'Moe'\">Moe</span>\n  <span *ngSwitchDefault>other</span>\n</span>\n  "
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
--- template-syntax/app/app.component.ts
+++ template-syntax/app/app.component.ts
@@ -11,15 +11,54 @@ import {Component} from 'angular2/core';
   <p>The sum of 1 + 1 is {{1 + 1}} + </p>
 
   <!-- "The sum of 1 + 1 is not 4" -->
-  <p>The sum of 1 + 1 is not {{1 + 1 + " " + getVal()}}</p>
-  <button [disabled]="isUnchanged">Save</button>
+  <p>The sum of 1 + 1 is not {{2 + 2 + " " + getVal()}}</p>
+  <button (click)="toggleSelected()">Save</button>
+  <div id="testNgClass1" [ngClass] = "{selected: isSelected, huynh: !isSelected, abc: !isSelected}">1</div>
+  <div class="abc" [class.trung]="isSelected"></div>
+  <button [title]="tittle">help</button>
+
+<!-- isSpecial is true -->
+<div [class.hidden]="!isSpecial">Show with class</div>
+<div [class.hidden]="isSpecial">Hide with class</div>
+
+<!-- HeroDetail is in the DOM but hidden -->
+<hero-detail [class.hidden]="isSpecial"></hero-detail>
+
+<div [style.display]="isSpecial ? 'block' : 'none'">Show with style</div>
+<div [style.display]="isSpecial ? 'none'  : 'block'">Hide with style</div>
+
+<span [ngSwitch]="toeChoice">
+  <span *ngSwitchWhen="'Eenie'">Eenie</span>
+  <span *ngSwitchWhen="'Meanie'">Meanie</span>
+  <span *ngSwitchWhen="'Miney'">Miney</span>
+  <span *ngSwitchWhen="'Moe'">Moe</span>
+  <span *ngSwitchDefault>other</span>
+</span>
   `
 })
 
 export class AppComponent {
   public heroImageUrl = "http://ww1.prweb.com/prfiles/2014/04/10/11752526/gI_134971_best-image-web-hosting.png";
   public tittle = "Template syntax";
-  getVal() {
-    return "Message";
+
+  isSelected: boolean;
+
+  toggleSelected(){
+    this.isSelected = !this.isSelected;
+    console.log("this.isSelected:", this.isSelected);
   }
+  getVal() {
+    var a = 40 + 4, b = "Text string";
+    return "Message";
+
+  }
+  // setClasses() {
+  //   let classes = {
+  //     saveable: this.canSave,      // true
+  //     modified: !this.isUnchanged, // false
+  //     special: this.isSpecial,     // true
+  //   }
+  //   return classes;
+  // }
 }
+
