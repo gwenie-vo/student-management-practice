1173a7e734c44e77ebb82de8429045eda805ab12
Huynh Vo <huynh.vo@asnet.com.vn>
2016-03-07 18:22:18 +0700

    Routing and navigation example

---
+++ routing-navigation/app/main.ts
@@ -0,0 +1,9 @@
+import {AppComponent} from './app.component';
+import {bootstrap} from 'angular2/platform/browser';
+import {ROUTER_PROVIDERS} from 'angular2/router';
+
+bootstrap(AppComponent, [
+  ROUTER_PROVIDERS
+]);
+
+
