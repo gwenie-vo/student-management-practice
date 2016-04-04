861e010813ff142cc000cd3f845b1c73bd8bddfa
Huynh Vo <huynh.vo@asnet.com.vn>
2016-04-04 16:57:29 +0700

    Initialize todo app

---
+++ todo-list/app/app.component.ts
@@ -0,0 +1,12 @@
+import {Component} from 'angular2/core';
+
+@Component ({
+  selector: 'my-app',
+  templateUrl: "./app/app.component.html"
+})
+
+
+export class AppComponent {
+  title = "Todos Management";
+}
+
