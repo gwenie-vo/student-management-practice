f20f0736b72361f7f980a7ae344d52e2e06c2a55
Huynh Vo <huynh.vo@asnet.com.vn>
2016-04-07 17:24:05 +0700

    Push new todo to the array

--- todo-list/app/todo/todo-add.component.ts
+++ todo-list/app/todo/todo-add.component.ts
@@ -1,14 +1,20 @@
 import {Component} from 'angular2/core';
 import {Todo, TodoStatus}      from './todo';
+import {TodoService}      from './todo.service';
 
 @Component({
   selector: 'todo-add-component',
-  templateUrl: "./app/todo/todo-add.component.html"
+  templateUrl: "./app/todo/todo-add.component.html",
+  providers: [TodoService]
 })
 
 export class TodoAddComponent {
   todo: Todo;
+  todoList;
 
+  constructor(private _todoService: TodoService) {
+    this.todoList = _todoService.getTodoList();
+  }
   /*
    * add new todo
    */
@@ -19,5 +25,8 @@ export class TodoAddComponent {
     this.todo.status = TodoStatus.ACTIVE;
     this.todo.id = Math.floor(100000000 + Math.random() * 900000000);
     console.log("TODO", this.todo);
+
+    this.todoList.push(this.todo);
+    console.log("new todoList array", this.todoList);
   }
 }
