5a6f859041b36e067403a6ee712dff0db5330ac4
Huynh Vo <huynh.vo@asnet.com.vn>
2016-04-08 18:00:10 +0700

    import http

--- todo-list/app/todo/todo.service.ts
+++ todo-list/app/todo/todo.service.ts
@@ -1,6 +1,8 @@
 import {Injectable} from 'angular2/core';
+import {Http, Response} from 'angular2/http';
+import {Observable}     from 'rxjs/Rx';
 import {Todo}       from './todo';
-import {TODOLIST}   from './mock-data';
+// import {TODOLIST}   from './mock-data';
 
 @Injectable()
 export class TodoService {
